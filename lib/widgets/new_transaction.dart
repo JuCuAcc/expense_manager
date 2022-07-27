1e1b7806b7b9e77d01c86d81371ffea68fb3d070
MD. Jashim Uddin <ju.cu.acc@outlook.com>
2022-07-27 15:13:11 +0600

    Basic End

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,8 +1,7 @@
-import 'dart:io';
+/// import 'dart:io';
 
 import 'package:expense_manager/widgets/adaptive_button.dart';
-import 'package:flutter/cupertino.dart';
-import 'dart:math';
+/// import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
