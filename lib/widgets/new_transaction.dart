f892b590c2ca1d6d0d424078e23a17f8e27722a3
MD. Jashim Uddin <ju.cu.acc@outlook.com>
2022-07-30 12:32:08 +0600

    initState Added

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -10,16 +10,46 @@ class NewTransaction extends StatefulWidget {
 
   final Function addTx;
 
-  NewTransaction(this.addTx);
+  NewTransaction(this.addTx){
+    print('Constructor NewTransaction Widget');
+  }
 
   @override
-  State<NewTransaction> createState() => _NewTransactionState();
+  State<NewTransaction> createState() {
+    print('createState NewTransaction Widget');
+    return _NewTransactionState();
+  } 
 }
 
 class _NewTransactionState extends State<NewTransaction> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime _selectedDate;
+
+
+  _NewTransactionState() {
+    print('Constructor NewTransaction State');
+  }
+
+  @override
+  void initState() {
+    print('initState()');     /// Call super initState first
+    super.initState();
+  }
+
+
+
+  @override
+  void didUpdateWidget(covariant NewTransaction oldWidget) {
+    print('didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    print('dispose()');
+    super.dispose();
+  }
 
   void _submitData(){
     if (_amountController.text.isEmpty){
